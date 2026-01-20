5a61d438da7fc1a2ee009fac04c6b59bd53621ac
Ry-ll <andresquintanaort@gmail.com>
2026-01-19 20:25:14 -0600

    Update ExampleModClient.java

--- src/client/java/com/example/ExampleModClient.java
+++ src/client/java/com/example/ExampleModClient.java
@@ -1,10 +1,24 @@
 package com.example;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderEvents;
+import net.minecraft.core.BlockPos;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.Map;
 
 public class ExampleModClient implements ClientModInitializer {
-	@Override
-	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-	}
+    
+    public static final Map<BlockPos, Integer> DISCOVERED_CHESTS = new ConcurrentHashMap<>();
+
+    @Override
+    public void onInitializeClient() {
+      
+		WorldRenderEvents.END.register(context -> {
+            if (DISCOVERED_CHESTS.isEmpty()) return;
+
+            DISCOVERED_CHESTS.forEach((pos, type) -> {
+    
+            });
+        });
+    }
 }
